7142498fbe2ced55e786c7113832a27c00a6b67f
palflaszlo <pallaszlo15@gmail.com>
2020-02-06 16:42:59 +0100

    :D

--- Helsinki2017/Versenyzo.cs
+++ Helsinki2017/Versenyzo.cs
@@ -19,6 +19,19 @@ namespace Helsinki2017
         double komponens2;
         int levonas2;
 
+        public double ÖsszPontszám
+        {
+            get
+            {
+                return technika + komponens - levonas + technika2 + komponens2 - levonas2;
+            }
+        }
+
+        public double OsszPontszam()
+        {
+            return technika + komponens - levonas + technika2 + komponens2 - levonas2;
+        }
+
         public Versenyzo(string nev, string orszag, double technika, double komponens, int levonas)
         {
             this.nev = nev;
@@ -26,6 +39,9 @@ namespace Helsinki2017
             this.technika = technika;
             this.komponens = komponens;
             this.levonas = levonas;
+            this.technika2 = 0;
+            this.komponens2 = 0;
+            this.levonas2 = 0;
         }
 
         public string Nev { get => nev; set => nev = value; }
@@ -33,5 +49,8 @@ namespace Helsinki2017
         public double Technika { get => technika; set => technika = value; }
         public double Komponens { get => komponens; set => komponens = value; }
         public int Levonas { get => levonas; set => levonas = value; }
+        public double Technika2 { get => technika2; set => technika2 = value; }
+        public double Komponens2 { get => komponens2; set => komponens2 = value; }
+        public int Levonas2 { get => levonas2; set => levonas2 = value; }
     }
 }
